32e27c2fe64dc1e642c11a748f435cc7a1b1cd20
younes <younes3021.us@gmail.com>
2025-02-28 20:09:58 +0330

    some changes for css

--- onlinegym/onlinegym/settings.py
+++ onlinegym/onlinegym/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
