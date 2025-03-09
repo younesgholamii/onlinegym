9f1052213c38e7b64c371007d3e95da3e73243f9
younes <younes3021.us@gmail.com>
2025-03-10 02:07:30 +0330

    search box added

--- onlinegym/home/views.py
+++ onlinegym/home/views.py
@@ -5,5 +5,10 @@ from coaches.models import CoachPosts
 
 class HomeView(View):
     def get(self, request):
-        posts = CoachPosts.objects.all()
+        coach_username = request.GET.get('coach_username')
+
+        if coach_username:
+            posts = CoachPosts.objects.filter(coach__user__username__icontains=coach_username)
+        else:
+            posts = CoachPosts.objects.all()
         return render(request, 'home/home.html', {'posts': posts})
